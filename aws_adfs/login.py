532fbdfea59484a76e085cf26400187aca3c7efe
Michael McGarrah <michael.mcgarrah@bcbsnc.com>
2019-08-27 19:36:09 -0400

    Quick hack to test the AWS Console URL generation.

--- aws_adfs/login.py
+++ aws_adfs/login.py
@@ -12,6 +12,7 @@ from . import authenticator
 from . import prepare
 from . import role_chooser
 
+import requests, json, sys, urllib
 
 @click.command()
 @click.option(
@@ -197,6 +198,43 @@ def login(
         SAMLAssertion=assertion,
         DurationSeconds=int(config.session_duration),
     )
+
+    # Format resulting temporary credentials into JSON
+    url_credentials = {}
+    url_credentials['sessionId'] = aws_session_token.get('Credentials').get('AccessKeyId')
+    url_credentials['sessionKey'] = aws_session_token.get('Credentials').get('SecretAccessKey')
+    url_credentials['sessionToken'] = aws_session_token.get('Credentials').get('SessionToken')
+    json_string_with_temp_credentials = json.dumps(url_credentials)
+
+    # Make request to AWS federation endpoint to get sign-in token.
+    # Construct the parameter string with the sign-in action request,
+    # a 12-hour session duration, and the JSON document with temporary credentials
+    request_parameters = "?Action=getSigninToken"
+    request_parameters += "&SessionDuration=43200"
+    if sys.version_info[0] < 3:
+        def quote_plus_function(s):
+            return urllib.quote_plus(s)
+    else:
+        def quote_plus_function(s):
+            return urllib.parse.quote_plus(s)
+    request_parameters += "&Session=" + quote_plus_function(json_string_with_temp_credentials)
+    request_url = "https://signin.aws.amazon.com/federation" + request_parameters
+    r = requests.get(request_url)
+    # Returns a JSON document with a single element named SigninToken.
+    signin_token = json.loads(r.text)
+
+    # Create URL where users can use the sign-in token to sign in to
+    # the console. This URL must be used within 15 minutes after the
+    # sign-in token was issued.
+    request_parameters = "?Action=login" 
+    request_parameters += "&Issuer=" + config.adfs_host
+    request_parameters += "&Destination=" + quote_plus_function("https://console.aws.amazon.com/")
+    request_parameters += "&SigninToken=" + signin_token["SigninToken"]
+    request_url = "https://signin.aws.amazon.com/federation" + request_parameters
+
+    # Final URL
+    #print(request_url)
+    click.echo( u"""AWS Console URL for {}: {}""".format(config.profile, request_url) )
 
     if stdout:
         _emit_json(aws_session_token)
